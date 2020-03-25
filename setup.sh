acbf3761dac16eca796035454faf7db9c95adaf5
BuildTools <unconfigured@null.spigotmc.org>
2020-03-24 20:40:09 -0400

    changed to my plugin download

--- setup.sh
+++ setup.sh
@@ -6,11 +6,11 @@ sudo apt-get install -y build-essential python-pip
 sudo apt-get install -y python-setuptools
 sudo pip install wheel
 pip install pycryptodomex
-sudo apt install kodi-inputstream-adaptive
+sudo apt install -y kodi-inputstream-adaptive
 wget https://github.com/castagnait/repository.castagnait/raw/master/repository.castagnait-1.0.0.zip
 
 # Install Steam link and the kodi addon for it
-wget https://github.com/bigbrozer/kodi-steamlink-launcher/releases/download/v0.0.2/kodi-steamlink-launcher-v0.0.2.zip
+wget https://github.com/tobnap/kodi-steamlink-launcher/releases/download/v0.0.2/kodi-steamlink-launcher-v0.0.2.zip
 
 cat <<EOF | sudo tee /etc/systemd/system/kodi.service
 [Unit]
